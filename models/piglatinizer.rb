d06061ee4d1b9d4d287bf26c88a27eb4aca3a3cc
John Hayes <johnnyhayes7@gmail.com>
2019-04-30 22:40:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,6 @@ class PigLatinizer
 attr_accessor :text 
   
     
-    
     def piglatinize(text)
       word_ary = text.split(" ")
       w = word_ary.map{|word| pig_word(word)}
@@ -15,15 +14,15 @@ attr_accessor :text
       if vowels.include?(word[0])
         "#{word}way"
       else
-        consonants << word[0]
-        if !vowels.include?(word[1])
-          consonants << word[1]
-          if !vowels.include?(word[2])
-            consonants << word[2]
+        consonants << word[0].downcase
+        if !vowels.include?(word[1].downcase)
+          consonants << word[1].downcase
+          if !vowels.include?(word[2].downcase)
+            consonants << word[2].downcase
           end
           # binding.pry
         end
-        "#{word[consonants.length..-1] + consonants.join + "ay"}"
+        "#{word[consonants.length..-1] + consonants.join+"ay"}"
       end
     end
    
